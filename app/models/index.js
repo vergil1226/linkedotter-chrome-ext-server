6e757c76ab2c6f1e2e298b736981a5cd67ef3f9b
john1226 <devil.vergil1226@gmail.com>
2023-06-22 07:33:10 -0700

    team feature added

--- app/models/index.js
+++ app/models/index.js
@@ -3,4 +3,5 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.user = require("./user.model"); 
+db.team = require("./team.model");
 module.exports = db;
--- app/models/user.model.js
+++ app/models/user.model.js
@@ -5,8 +5,8 @@ const User = mongoose.model(
   new mongoose.Schema({
     username: String,
     email: String,
-    password: String 
-    
+    password: String,
+    team_id: String,
   })
 );
 
